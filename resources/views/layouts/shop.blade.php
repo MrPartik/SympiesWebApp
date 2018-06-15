73facfdca4bb9d894179466a23d088f75a8a2ed8
MRPartik <loyolapat04@gmail.com>
2018-06-15 23:36:34 +0800

    add laravolt/avatar

--- resources/views/layouts/shop.blade.php
+++ resources/views/layouts/shop.blade.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!Doctype html>
 <html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
@@ -16,7 +16,7 @@
         <!-- Logo -->
         <a href="#" class="logo">
             <!-- mini logo for sidebar mini 50x50 pixels -->
-            <span class="logo-mini"><b>A</b>LT</span>
+            <span class="logo-mini"><img src="{{ Avatar::create('Sympies')->toBase64() }}" style="height: 40px;"></span>
             <!-- logo for regular state and mobile devices -->
             <span class="logo-lg"><b>Sympies</b></span>
         </a>
@@ -24,7 +24,10 @@
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top">
             <!-- Sidebar toggle button-->
-
+            <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+                <span class="sr-only">Toggle navigation</span>
+            </a>
+            <!-- Navbar Right Menu -->
             <div class="navbar-custom-menu">
                 <ul class="nav navbar-nav">
                     <!-- Messages: style can be found in dropdown.less-->
@@ -41,7 +44,7 @@
                                     <li><!-- start message -->
                                         <a href="#">
                                             <div class="pull-left">
-                                                <img src="../../dist/img/user2-160x160.jpg" class="img-circle"
+                                                <img src="{{ Avatar::create(Auth::user()->name)->toBase64() }}" class="img-circle"
                                                      alt="User Image">
                                             </div>
                                             <h4>
@@ -115,13 +118,13 @@
                     <!-- User Account: style can be found in dropdown.less -->
                     <li class="dropdown user user-menu">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <img src="../../dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+                            <img src="{{ Avatar::create(Auth::user()->name)->toBase64() }}" class="user-image" alt="User Image">
                             <span class="hidden-xs">{{Auth::user()->name}}</span>
                         </a>
                         <ul class="dropdown-menu">
                             <!-- User image -->
                             <li class="user-header">
-                                <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                                <img src="{{ Avatar::create(Auth::user()->name)->toBase64() }}" class="img-circle" alt="User Image">
 
                                 <p>
                                     {{Auth::user()->name}} - Web Developer
@@ -162,28 +165,40 @@
             <!-- Sidebar user panel -->
             <div class="user-panel">
                 <div class="pull-left image">
-                    <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                    <img src="{{ Avatar::create(Auth::user()->name)->toBase64() }}" class="img-circle" alt="User Image">
                 </div>
                 <div class="pull-left info">
                     <p>{{auth::user()->name}}</p>
+                    <a href="#"><i class="fa fa-tag text-info"></i> {{auth::user()->email}}</a>
                 </div>
-            </div>
+            </div> <!-- search form -->
+            <form action="#" method="get" class="sidebar-form">
+                <div class="input-group">
+                    <input type="text" name="q" class="form-control" placeholder="Search...">
+                    <span class="input-group-btn">
+                <button type="submit" name="search" id="search-btn" class="btn btn-flat">
+                  <i class="fa fa-search"></i>
+                </button>
+              </span>
+                </div>
+            </form>
+            <!-- /.search form -->
             <!-- sidebar menu: : style can be found in sidebar.less -->
             <ul class="sidebar-menu" data-widget="tree">
                 <li class="header">MAIN NAVIGATION</li>
                 <li class="{{Route::is('Dashboard')?'active':''}}"><a href="{{route('Dashboard')}}"><i
-                                class="fa fa-dashboard"></i> Dashboard</a></li>
+                                class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
                 <li class="treeview {{Route::is('Ledger','Inventory','Customer')?'active':''}}">
                     <a href="#">
-                        <i class="fa fa-tags "></i>
+                        <i class="fa fa-shopping-cart"></i>
                         <span>Shop</span>
                         <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
                     </a>
                     <ul class="treeview-menu">
-                        <li class="{{Route::is('Ledger')?'active':''}}"><a href="{{route('Ledger')}}"><i
-                                        class="fa fa-book "></i> General Ledger</a></li>
+                        <li class="{{Route::is('Ledger')?'active':''}}"><a href="{{route('Ledger')}}">
+                                <i class="fa fa-book"></i> General Ledger</a></li>
                         <li class="{{Route::is('Inventory')?'active':''}}"><a href="{{route('Inventory')}}"><i
                                         class="fa fa-bar-chart-o "></i> Inventory</a></li>
                         <li class="{{Route::is('Customer')?'active':''}}"><a href="{{route('Customer')}}"><i
@@ -193,7 +208,7 @@
                 <li class="treeview">
                     <a href="#">
                         <i class="fa fa-users"></i>
-                        <span>Users</span>
+                        <span>User Management</span>
                         <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
@@ -272,9 +287,7 @@
     </div>
 </div>
 <!-- End of Settings -->
-<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-    {{ csrf_field() }}
-</form>
+
 <!-- End of Modals -->
 </body>
 @include('includes.js')
